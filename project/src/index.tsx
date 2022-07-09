774d2bd1af30e574847884c922e707796542123a
Alena Karenina <karenina@corp.profi.travel>
2022-07-09 22:35:50 +0500

    Добавит React-компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const FILM_CARD = {
+  TITLE: 'Thor: Love and Thunder',
+  GENRE: 'Adventure',
+  YEAR: 2022,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={FILM_CARD.TITLE}
+      genre={FILM_CARD.GENRE}
+      year={FILM_CARD.YEAR}
+    />
   </React.StrictMode>,
 );
