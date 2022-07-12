14a0e9795348f805a465ea5963af466beec80de3
Alena Karenina <karenina@corp.profi.travel>
2022-07-12 20:57:20 +0500

    добавит маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import MoviePageInListScreen from '../../pages/movie-page-in-list-screen/movie-page-in-list-screen';
+//import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   title: string,
@@ -8,11 +16,39 @@ type AppScreenProps = {
 
 function App({title, genre, year}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen
-      title={title}
-      genre={genre}
-      year={year}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen title={title} genre={genre} year={year} />}
+        />
+
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePageInListScreen />}
+        />
+
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewScreen />}
+        />
+
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignInScreen />}
+        />
+
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,10 @@
+function NotFoundScreen(): JSX.Element {
+  return (
+    <section className="user-block">
+      <h1>404. Page not found</h1>
+      <a href="/" className="user-block__link">Вернуться на главную</a>
+    </section>
+  );
+}
+
+export default NotFoundScreen;
