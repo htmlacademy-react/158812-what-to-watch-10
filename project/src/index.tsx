b5fc75f355acb0a338a684d085ab185d5ff5c38f
Alena Karenina <karenina@corp.profi.travel>
2022-07-26 22:43:44 +0500

    подключит redux к проекту

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {FILMS_DATA} from './mocks/films';
 import {reviews} from './mocks/reviews';
+import {store} from './store';
 
 const AppProps = {
   filmPromo: {
@@ -18,10 +20,12 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      filmPromo={AppProps.filmPromo}
-      films={FILMS_DATA}
-      reviews={reviews}
-    />
+    <Provider store = {store}>
+      <App
+        filmPromo={AppProps.filmPromo}
+        films={FILMS_DATA}
+        reviews={reviews}
+      />
+    </Provider>
   </React.StrictMode>,
 );
